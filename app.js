2eb910860a3c12f426e93ac291cb6ae6b2388ec5
kayro-is <luffykosuki@gmail.com>
2026-04-19 00:08:24 +0200

    mises a jour fichier env

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ const cors = require('cors');
 
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: [
+      "http://localhost:5173",
+      process.env.FRONTEND_URL,
+    ].filter(Boolean),
+    credentials: true,
   })
 );
 
